95ad472922629d6c7b09fc02c92d6d1b786e2f11
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-22 23:38:07 +0200

    Admin -Update Product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -30,3 +30,21 @@ export const getProductDetails = async (req, res) => {
         product,
     })
 };
+// Put product details => /api/v1/admin/products/:id
+export const updateProduct = async (req, res) => {
+    let product = await Product.findById(req?.params?.id)
+
+    if (!product) {
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+        new: true
+    })
+
+    res.status(200).json({
+        product,
+    })
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { getProductDetails, getProducts, newProduct } from "../controllers/productControllers.js";
+import { getProductDetails, getProducts, newProduct, updateProduct } from "../controllers/productControllers.js";
 
 router.route("/products").get(getProducts)
 router.route("/admin/products").post(newProduct)
 router.route("/products/:id").get(getProductDetails)
+router.route("/products/:id").put(updateProduct)
 
 export default router
