6639d475b49b4a4955bbc3914ee2afd10bd78135
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-22 21:06:08 +0200

    Get Single Product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -16,3 +16,17 @@ export const newProduct = async (req, res) => {
         product,
     })
 };
+// Get Single product details => /api/v1/admin/products/:id
+export const getProductDetails = async (req, res) => {
+    const product = await Product.findById(req?.params?.id)
+
+    if (!product) {
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    res.status(200).json({
+        product,
+    })
+};
