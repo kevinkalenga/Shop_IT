3ff9b7365d41a0d54cc051fa93bd53ab03b9d529
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-22 15:13:28 +0200

    Installing and Setting up Nodemon

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,5 @@ import dotenv from 'dotenv'
 dotenv.config({ path: "backend/config/config.env" })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT}`)
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
 })
