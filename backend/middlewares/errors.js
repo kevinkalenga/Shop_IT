d8e56b5c62ab4b2d7dffd56ce9152da23c811127
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-24 01:17:08 +0200

    Validation & Mongoose ID Error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,10 +1,21 @@
-
+import ErrorHandler from "../utils/errorHandler.js"
 
 export default (err, req, res, next) => {
     let error = {
         statusCode: err?.statusCode || 500,
         message: err?.message || "Internal Server Error",
     };
+
+    // Handle Invalid Mongoose ID Error 
+
+    if (error.name === 'CastError') {
+        const message = `Resource not found. Invalid: ${err?.path}`
+        error = new ErrorHandler(message, 404)
+    }
+    if (error.name === 'ValidationError') {
+        const message = Object.values(err.errors).map((value) => value.message)
+        error = new ErrorHandler(message, 400)
+    }
 
     // we show the stack in the development when there is an error
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
