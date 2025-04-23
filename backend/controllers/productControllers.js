1dac4ff4dc0fc3f7a9f10964c857cdc19767aeb1
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-24 01:04:18 +0200

    Catching Async Errors

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,24 +1,25 @@
 import Product from "../models/productModel.js";
 import ErrorHandler from "../utils/errorHandler.js"
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js"
 
 // Get all the products
-export const getProducts = async (req, res) => {
+export const getProducts = catchAsyncErrors(async (req, res) => {
     const products = await Product.find();
     res.status(200).json({
         products
     })
-};
+});
 
 // Create new product => /api/v1/admin/products
-export const newProduct = async (req, res) => {
+export const newProduct = catchAsyncErrors(async (req, res) => {
     const product = await Product.create(req.body)
 
     res.status(200).json({
         product,
     })
-};
+});
 // Get Single product details => /api/v1/products/:id
-export const getProductDetails = async (req, res, next) => {
+export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
     const product = await Product.findById(req?.params?.id)
 
     if (!product) {
@@ -28,15 +29,13 @@ export const getProductDetails = async (req, res, next) => {
     res.status(200).json({
         product,
     })
-};
+});
 // update product  => /api/v1/products/:id
-export const updateProduct = async (req, res) => {
+export const updateProduct = catchAsyncErrors(async (req, res) => {
     let product = await Product.findById(req?.params?.id)
 
     if (!product) {
-        return res.status(404).json({
-            error: "Product not found",
-        });
+        return next(new ErrorHandler("Produc not found", 404))
     }
     // The optional chaining ?. is a safe way to access nested object properties, even if an intermediate property doesn’t exist.
     product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
@@ -46,17 +45,15 @@ export const updateProduct = async (req, res) => {
     res.status(200).json({
         product,
     })
-};
+});
 
 // delete product  => /api/v1/products/:id
-export const deleteProduct = async (req, res) => {
+export const deleteProduct = catchAsyncErrors(async (req, res) => {
     // search the product in the database
     const product = await Product.findById(req?.params?.id)
 
     if (!product) {
-        return res.status(404).json({
-            error: "Product not found",
-        });
+        return next(new ErrorHandler("Produc not found", 404))
     }
 
     await product.deleteOne()
@@ -64,4 +61,4 @@ export const deleteProduct = async (req, res) => {
     res.status(200).json({
         message: "Product deleted"
     })
-};
+});
