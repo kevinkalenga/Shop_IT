d0cb4ea3e55836118a2f669a1bd4b64a5623568a
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-24 00:37:38 +0200

    Production vs Development Errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -6,7 +6,19 @@ export default (err, req, res, next) => {
         message: err?.message || "Internal Server Error",
     };
 
-    res.status(error.statusCode).json({
-        message: error.message,
-    });
+    // we show the stack in the development when there is an error
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: err,
+            stack: err?.stack,
+        });
+    }
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+        res.status(error.statusCode).json({
+            message: error.message,
+        });
+    }
+
+
 };
