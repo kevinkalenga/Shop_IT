4e14fd01d7bdca17f6143b5014a9f1310373c566
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-25 01:45:19 +0200

    Login User and Assign Token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,7 +1,9 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/userModel.js"
+import ErrorHandler from "../utils/errorHandler.js"
 
 
+// Register user => /api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
     const { name, email, password } = req.body;
 
@@ -17,3 +19,31 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
         token,
     })
 })
+// Login user => /api/v1/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === "" || password === "") {
+        return next(new ErrorHandler('Please enter email and password', 400))
+    }
+
+    // Find user in the database  
+    const user = await User.findOne({ email }).select("+password")
+
+    if (!user) {
+        return next(new ErrorHandler('Invalid email or password', 401))
+    }
+
+    //  check if password is correct
+    const isPasswordMatched = await user.comparePassword(password)
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid email or password', 401))
+    }
+
+    const token = user.getJwtToken()
+
+    res.status(201).json({
+        token,
+    })
+})
+
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { registerUser } from "../controllers/authControllers.js";
+import { registerUser, loginUser } from "../controllers/authControllers.js";
 
 
 router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
 
 
 export default router
