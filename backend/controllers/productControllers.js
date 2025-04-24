69cd5ec1282af27e2571ea3efb64aed1bc21bee2
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-24 03:22:18 +0200

    Filter Products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,13 +2,17 @@ import Product from "../models/productModel.js";
 import ErrorHandler from "../utils/errorHandler.js"
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import APIFilters from "../utils/apiFilter.js"
+import qs from 'qs';
 
 // Get all the products
 export const getProducts = catchAsyncErrors(async (req, res) => {
+    const parsedQuery = qs.parse(req._parsedUrl.query); // on parse manuellement
     // we have to pass the query and queryStr from the constructor
-    const apiFilters = new APIFilters(Product, req.query).search()
+    console.log("Requête reçue :", req.query);
+    // const apiFilters = new APIFilters(Product.find(), req.query).search().filters()
+    const apiFilters = new APIFilters(Product.find(), parsedQuery).search().filters()
 
-    let products = await apiFilters.query;
+    let products = await apiFilters.getQuery();
     let filteredProductsCount = products.length
 
     res.status(200).json({
--- backend/utils/apiFilter.js
+++ backend/utils/apiFilter.js
@@ -17,6 +17,39 @@ class APIFilters {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filters() {
+        const queryCopy = { ...this.queryStr };
+
+        // Fields to remove because we've already got the keyword in the search 
+        const fieldsToRemove = ["keyword"]
+        fieldsToRemove.forEach((el) => delete queryCopy[el])
+
+
+        // Advance filter for price, ratings etc
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`)
+
+        let parsedQuery = JSON.parse(queryStr)
+
+        // Convertir les valeurs en nombre si c'est un filtre numérique
+        for (const key in parsedQuery) {
+            if (typeof parsedQuery[key] === 'object') {
+                for (const op in parsedQuery[key]) {
+                    const val = parsedQuery[key][op];
+                    if (!isNaN(val)) parsedQuery[key][op] = Number(val);
+                }
+            }
+        }
+
+
+        this.query = this.query.find(parsedQuery)
+        return this
+    }
+
+    getQuery() {
+        return this.query;
+    }
 }
 
 export default APIFilters
