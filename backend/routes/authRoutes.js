fc726372066d5c60ec69b2d958a50a8bae4105ea
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-25 02:40:09 +0200

    Logout User

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { registerUser, loginUser } from "../controllers/authControllers.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/authControllers.js";
 
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+router.route("/logout").get(logoutUser)
 
 
 export default router
