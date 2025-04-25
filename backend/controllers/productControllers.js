6b1981a2d2c07fba6ac012f6d6b286e30a8e19d9
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-26 01:01:51 +0200

    Adding User in Products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -30,7 +30,12 @@ export const getProducts = catchAsyncErrors(async (req, res) => {
 
 // Create new product => /api/v1/admin/products
 export const newProduct = catchAsyncErrors(async (req, res) => {
+
+    req.body.user = req.user._id
+
     const product = await Product.create(req.body)
+
+
 
     res.status(200).json({
         product,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from "../controllers/productControllers.js";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js"
 
-router.route("/products").get(isAuthenticatedUser, authorizeRoles("admin"), getProducts)
-router.route("/admin/products").post(newProduct)
+router.route("/products").get(getProducts)
+router.route("/admin/products").post(isAuthenticatedUser, authorizeRoles("admin"), newProduct)
 router.route("/products/:id").get(getProductDetails)
-router.route("/admin/products/:id").put(updateProduct)
-router.route("/admin/products/:id").delete(deleteProduct)
+router.route("/admin/products/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+router.route("/admin/products/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
 
 export default router
