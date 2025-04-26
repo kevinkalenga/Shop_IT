cd545402666f18fd0337e0e9c97bc97a9da5bbeb
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-26 04:08:56 +0200

    Handle Duplicate key and JWT Error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -12,10 +12,36 @@ export default (err, req, res, next) => {
         const message = `Resource not found. Invalid: ${err?.path}`
         error = new ErrorHandler(message, 404)
     }
+
+    // Handle validation Error
     if (error.name === 'ValidationError') {
         const message = Object.values(err.errors).map((value) => value.message)
         error = new ErrorHandler(message, 400)
     }
+
+    // Handle Invalid Mongoose Duplicate Key Error
+
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered.`
+        error = new ErrorHandler(message, 400)
+    }
+
+    // Handle wrong JWT Error 
+
+    if (error.name === 'JsonWebTokenError') {
+        const message = `JSON Web Token is invalid. Try Again!!!`;
+        error = new ErrorHandler(message, 400)
+    }
+    // Handle expired JWT Error 
+
+    if (error.name === 'TokenExpiredError') {
+        const message = `JSON Web Token is expired. Try Again!!!`;
+        error = new ErrorHandler(message, 400)
+    }
+
+
+
+
 
     // we show the stack in the development when there is an error
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
