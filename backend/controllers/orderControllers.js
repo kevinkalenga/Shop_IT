427b8c4bfc2c801045277ad305afd75af58f4f50
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-28 12:21:46 +0200

    Delete Order

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -104,3 +104,19 @@ export const updateOrder = catchAsyncErrors(async (req, res, next) => {
         success: true,
     })
 });
+
+// Get order details => /api/v1/admin/orders/:id
+
+export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if (!order) {
+        return next(new ErrorHandler("No order found with this ID", 404));
+    }
+
+    await order.deleteOne()
+
+    res.status(200).json({
+        success: true,
+    })
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
-import { newOrder, getOrderDetails, myOrders, updateOrder, allOrders } from "../controllers/orderControllers.js"
+import { newOrder, getOrderDetails, myOrders, updateOrder, allOrders, deleteOrder } from "../controllers/orderControllers.js"
 
 const router = express.Router();
 
@@ -9,6 +9,8 @@ router.route('/orders/:id').get(isAuthenticatedUser, getOrderDetails)
 router.route('/me/orders').get(isAuthenticatedUser, myOrders)
 router.route('/admin/orders').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders)
 router.route('/admin/orders/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder)
+
 
 
 export default router
