38c6a42525e18d64c3beaf1edacb2962b55d3955
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-29 11:31:32 +0200

    Implementing React Router DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import Home from './components/Home'
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <Home />
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+
+        </div>
+
+        <Footer />
+
       </div>
-
-      <Footer />
-
-    </div>
+    </Router>
   );
 }
 
