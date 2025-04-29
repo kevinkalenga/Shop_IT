07c6f26eda85aaf3199d1d770937a0b82dd6429e
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-29 14:14:19 +0200

    Create Redux Toolkit Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./redux/store"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
+
     </HelmetProvider>
 
   </React.StrictMode>
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// create store which is going to be use in our entry point (index.js)
+
+export const store = configureStore({
+    reducer: {},
+})
