8fedbd29f535b42d6e3b88dae84169245999ff00
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-29 10:44:25 +0200

    Installing React and Cleaning Project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-danger bg-info">Shop IT</h1>
+
+    </div>
+  );
+}
+
+export default App;
