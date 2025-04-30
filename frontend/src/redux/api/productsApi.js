555a1e2db2d4f10dbdfa18ee37cc6faffd635d36
kevin kalenga <kevinkalenga10@gmail.com>
2025-04-30 11:16:11 +0200

    Fetch all Products Query

---
+++ frontend/src/redux/api/productsApi.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// create api 
+export const productApi = createApi({
+    reducerPath: "productApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "/api/v1"
+    }),
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: (params) => "/products",
+        }),
+    }),
+});
+
+export const { useGetProductsQuery } = productApi;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { productApi } from "./api/productsApi"
 
 // create store which is going to be use in our entry point (index.js)
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [productApi.reducerPath]: productApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat([productApi.middleware]),
 })
