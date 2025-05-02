16064b7790a3905d998e86521367206e92fe4eda
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-02 10:45:46 +0200

    Search Products Component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,14 +7,15 @@ import toast from 'react-hot-toast';
 import CustomPagination from "./layout/CustomPagination"
 import { useSearchParams } from "react-router-dom";;
 
-
+// <div className={keyword ? "col-12 col-sm-9": "col-md-12"}>
 const Home = () => {
 
     let [searchParams] = useSearchParams();
     // if page is not there that will be the first page
     const page = searchParams.get("page") || 1;
+    const keyword = searchParams.get("keyword") || ""
 
-    const params = { page }
+    const params = { page, keyword }
 
     const { data, isLoading, error, isError } = useGetProductsQuery(params);
     console.log(data)
@@ -25,6 +26,8 @@ const Home = () => {
         }
     }, [isError])
 
+    const columnSize = keyword ? 4 : 3
+
     if (isLoading) return <Loader />
 
 
@@ -34,16 +37,25 @@ const Home = () => {
             <MetaData title="Buy Best Product Online" />
 
             <div className="row">
+                {keyword && (
+                    <div className='col-6 col-md-3 mt-5'>
+                        <p>FILTERS</p>
+                    </div>
+                )}
 
-                <div className="col-12 col-sm-6 col-md-12">
-                    <h1 id="products_heading" className="text-secondary">Latest Products</h1>
+                <div className={keyword ? "col-6 col-md-9" : "col-6 col-md-12"}>
+                    <h1 id="products_heading" className="text-secondary">
+                        {keyword ? `${data?.products.length} Products found with keyword: ${keyword}`
+                            : "Latest Products"}
+
+                    </h1>
 
                     <section id="products" className="mt-5">
                         <div className="row">
                             {/* <!-- Product Item 1 --> */}
 
                             {data?.products?.map((product) => (
-                                <ProductItem product={product} />
+                                <ProductItem product={product} columnSize={columnSize} />
                             ))}
 
 
--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -12,7 +12,8 @@ export const productApi = createApi({
             query: (params) => ({
                 url: "/products",
                 params: {
-                    page: params?.page
+                    page: params?.page,
+                    keyword: params?.keyword
                 }
             }),
 
